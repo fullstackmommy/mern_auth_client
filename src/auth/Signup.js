a0881e3568e5b917037f87c6122155fedad7d775
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-07-08 10:11:58 +0800

    feature: signup with toast notification

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -6,10 +6,92 @@ import "react-toastify/dist/ReactToastify.min.css";
 import Layout from "../core/Layout";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "nicole",
+    email: "nicole.budiman@gmail.com",
+    password: "qwqwqwqw",
+    buttonText: "Submit"
+  });
+
+  const { name, email, password, buttonText } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: "Submitting" });
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_API}/signup`,
+      data: { name, email, password }
+    })
+      .then(response => {
+        console.log("SIGNUP SUCCESS", response);
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          buttonText: "Submitted"
+        });
+        toast.success(response.data.message);
+      })
+      .catch(error => {
+        console.log("SIGNUP ERROR", error.response.data);
+        setValues({
+          ...values,
+          buttonText: "Submit"
+        });
+        toast.error(error.response.data.error);
+      });
+  };
+
+  const signupForm = () => (
+    <form>
+      <div className="form-group">
+        <table className="text-muted">Name</table>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          value={name}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <table className="text-muted">Email</table>
+        <input
+          onChange={handleChange("email")}
+          type="text"
+          value={email}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <table className="text-muted">Password</table>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          value={password}
+          className="form-control"
+        />
+      </div>
+      <div>
+        <button className="btn btn-primary" onClick={clickSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
+
   return (
     <Layout>
-      <ToastContainer />
-      <h1>Signup</h1>
+      <div className="col-md-6 offset-md-3">
+        <ToastContainer />
+        <h1>Signup</h1>
+        {signupForm()}
+      </div>
     </Layout>
   );
 };
