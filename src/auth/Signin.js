697d507944bf4df3abbfbf4e8f2b9f5fe4bd717f
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-07-08 10:21:56 +0800

    feature: sign-in route

---
+++ src/auth/Signin.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import Layout from "../core/Layout";
+
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    buttonText: "Submit"
+  });
+
+  const { email, password, buttonText } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: "Submitting" });
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_API}/signin`,
+      data: { email, password }
+    })
+      .then(response => {
+        console.log("SIGN-IN SUCCESS", response);
+        setValues({
+          ...values,
+          email: "",
+          password: "",
+          buttonText: "Submitted"
+        });
+        toast.success(`Hey, ${response.data.user.name}, welcome back!`);
+      })
+      .catch(error => {
+        console.log("SIGN-IN ERROR", error.response.data);
+        setValues({
+          ...values,
+          buttonText: "Submit"
+        });
+        toast.error(error.response.data.error);
+      });
+  };
+
+  const signinForm = () => (
+    <form>
+      <div className="form-group">
+        <table className="text-muted">Email</table>
+        <input
+          onChange={handleChange("email")}
+          type="text"
+          value={email}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <table className="text-muted">Password</table>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          value={password}
+          className="form-control"
+        />
+      </div>
+      <div>
+        <button className="btn btn-primary" onClick={clickSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
+
+  return (
+    <Layout>
+      <div className="col-md-6 offset-md-3">
+        <ToastContainer />
+        <h1>Signup</h1>
+        {signinForm()}
+      </div>
+    </Layout>
+  );
+};
+
+export default Signin;
